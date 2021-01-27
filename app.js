c253d3c0ebee9d03193792e17cf817e6626bc3be
Lea Dalibard <leadalibard@gmail.com>
2021-01-27 14:54:48 +0100

    Adding default connection to Database

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://becode:becode@cluster0.oow3f.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error :'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
