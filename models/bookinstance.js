769eec7d9ea9f3c8cf9ca649f250d1b530e82d9a
Lea Dalibard <leadalibard@gmail.com>
2021-01-28 16:01:17 +0100

    Used library Luxon and made a virtual instance due_back_formatted in bookInstance model to display date in prettier way

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const {DateTime}=require("luxon");
 
 var Schema = mongoose.Schema;
 
@@ -21,6 +22,13 @@ BookInstanceSchema
     .virtual('url')
     .get(function(){
         return '/catalog/bookinstance'+this._id;
+    });
+
+//Virtual for due_back_formatted
+BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get(function(){
+        return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
     });
 
 //Export model
