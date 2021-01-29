a15a7a713ee4cbf4939aae78b08bca8da51e37b6
Lea Dalibard <leadalibard@gmail.com>
2021-01-29 11:21:19 +0100

    Add book instance details

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -21,7 +21,7 @@ var BookInstanceSchema = new Schema(
 BookInstanceSchema
     .virtual('url')
     .get(function(){
-        return '/catalog/bookinstance'+this._id;
+        return '/catalog/bookinstance/'+this._id;
     });
 
 //Virtual for due_back_formatted
