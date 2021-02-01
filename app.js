86c57d5e3002d2f024398a30fac18fc6a520ebbe
Lea Dalibard <leadalibard@gmail.com>
2021-02-01 10:35:04 +0100

    Adding Helmet

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 //Set up mongoose connection
@@ -22,11 +23,13 @@ mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db=mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error :'));
 
-app.use(compression()); //Compress all routes
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(compression()); //Compress all routes
+app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
